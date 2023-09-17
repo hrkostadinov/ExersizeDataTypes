ccb222aab10b7fa189754a54e6e88edfe2d81db6
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-17 22:13:39 +0300

    task 09

---
+++ Data Types, Calculations/09. Aquarium/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _09._Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cmLength = int.Parse(Console.ReadLine());
+            int cmWidth = int.Parse(Console.ReadLine());
+            int cmHeight = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double volumeAquarium = cmLength * cmWidth * cmHeight;
+            double volumeLiters = volumeAquarium / 1000;
+            double space = percentage / 100;
+            double liters = volumeLiters * (1 - space);
+
+            Console.WriteLine($"{liters :f2}" );
+
+
+        }
+    }
+}
