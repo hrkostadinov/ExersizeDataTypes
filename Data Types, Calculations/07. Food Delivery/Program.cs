756e538f205847dc49d68b736ca9fe21c76aefc5
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-17 21:16:39 +0300

    task 07

---
+++ Data Types, Calculations/07. Food Delivery/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07._Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken = int.Parse(Console.ReadLine());
+            int fish = int.Parse(Console.ReadLine());
+            int vege = int.Parse(Console.ReadLine());
+
+            double priceChcken = chicken * 10.35;
+            double priceFish = fish * 12.40;
+            double priceVege = vege * 8.15;
+            double totalCoast = priceChcken + priceFish + priceVege;
+            double desert = totalCoast * 0.2;
+
+            double totalPrice = totalCoast + desert + 2.5;
+
+            Console.WriteLine(totalPrice);
+
+
+        }
+    }
+}
