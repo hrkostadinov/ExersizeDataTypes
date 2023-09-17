2a6ea2465caba77c11467882a93d3a04614fc656
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-17 21:37:13 +0300

    task 08

---
+++ Data Types, Calculations/08. Basketball Equipment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int trainingFee = int.Parse(Console.ReadLine());
+
+            double sneakers = trainingFee * 0.60;
+            double team = sneakers * 0.8;
+            double basketball = team * 0.25;
+            double accessories = basketball * 0.20;
+
+            double totalPrice = trainingFee + sneakers + team + basketball + accessories;
+
+            Console.WriteLine(totalPrice);
+
+        }
+    }
+}
