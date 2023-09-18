35c1e4cba4177b1f5507ed20a4c9b59824366faa
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-18 09:30:24 +0300

    task 01

---
+++ Conditional Statements. Nested Conditional Statements/01. Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _01._Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            double incom = 0.0;
+
+            if (type == "Premiere")
+            {
+                incom = rows * colums * 12.00;
+            }
+            else if (type == "Normal")
+            {
+                incom = rows * colums * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                incom = rows * colums * 5.00;
+            }
+
+            Console.WriteLine($"{incom:F2} leva");
+
+        }
+    }
+}
