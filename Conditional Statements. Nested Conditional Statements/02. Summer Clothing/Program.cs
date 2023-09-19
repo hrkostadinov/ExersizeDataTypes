be0cd17dc2014276e57748ce6bd6173b440e57c2
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-19 09:40:45 +0300

    task 02

---
+++ Conditional Statements. Nested Conditional Statements/02. Summer Clothing/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _02._Summer_Clothing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature = double.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            string clothing = "";
+            string shoes = "";
+
+            if (10 <= temperature && temperature <= 18)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    clothing = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            if (18 < temperature && temperature <= 24)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    clothing = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            if (temperature >= 25)
+            {
+                if (timeOfDay == "Morning")
+                {
+                    clothing = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (timeOfDay == "Afternoon")
+                {
+                    clothing = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                else if (timeOfDay == "Evening")
+                {
+                    clothing = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+
+            Console.WriteLine($"It's {temperature} degrees, get your {clothing} and {shoes}.");
+
+        }
+    }
+}
