354f6eb90286b3da46868ae08d5f1f322486eacc
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-24 15:19:30 +0300

    task 9

---
+++ Conditional Statements. Nested Conditional Statements/09. Ski Holiday/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _09._Ski_Holiday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string assessment = Console.ReadLine();
+
+            double nightPrice = 0;
+
+            switch(type)
+            {
+                case "room for one person":
+                    nightPrice = 118;
+                    break;
+                case "apartment":
+                    nightPrice = 155;
+                    if (days < 10)
+                        nightPrice *= 0.7;
+                    else if (days >= 10 && days <= 15)
+                        nightPrice *= 0.65;
+                    else if (days > 15)
+                        nightPrice *= 0.5;
+                    break;
+                case "president apartment":
+                    nightPrice = 235;
+                    if (days < 10)
+                        nightPrice *= 0.9;
+                    else if (days >= 10 && days <= 15)
+                        nightPrice *= 0.85;
+                    else if (days > 15)
+                        nightPrice *= 0.8;
+                    break;
+
+            }
+            double totalStayPrice = (days - 1) * nightPrice;
+            switch (assessment)
+            {
+                case "positive": totalStayPrice *= 1.25; break;
+                case "negative": totalStayPrice *= 0.9; break;
+
+            }
+            Console.WriteLine("{0:f2}", totalStayPrice);
+        }
+    }
+}
