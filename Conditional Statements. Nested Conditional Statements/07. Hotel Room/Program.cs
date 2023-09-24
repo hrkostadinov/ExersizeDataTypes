0aaaf8ac93d8d81766c80354f6d54b54ad682c94
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-24 12:28:43 +0300

    task 7

---
+++ Conditional Statements. Nested Conditional Statements/07. Hotel Room/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _07._Hotel_Room
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int nights = int.Parse(Console.ReadLine());
+
+            double studioPrice = 0;
+            double appPrice = 0;
+
+            switch (month)
+            {
+                case "May":
+                case "October":
+                    studioPrice = 50;
+                    appPrice = 65;
+                    break;
+                case "June":
+                case "September":
+                    studioPrice = 75.20;
+                    appPrice = 68.70;
+                    break;
+                case "July":
+                case "August":
+                    studioPrice = 76;
+                    appPrice = 77;
+                    break;
+
+            }
+            double studioTotal = studioPrice * nights;
+            double aptTotal = appPrice * nights;
+            if (nights > 14)
+            {
+                if (month == "May" || month == "October")
+                {
+                    studioTotal = studioTotal * 0.7;
+                }
+                else if (month == "June" || month == "September")
+                {
+                    studioTotal = studioTotal * 0.8;
+                }
+                aptTotal *= 0.9;
+
+            }
+            else if (nights >= 7)
+            {
+                if (month == "May" || month == "October")
+                    studioTotal = studioTotal * 0.95;
+            }
+
+            Console.WriteLine($"Apartment: {aptTotal:f2} lv.");
+            Console.WriteLine($"Studio: {studioTotal:f2} lv.");
+
+        }
+    }
+}
