79fb753fd64a48c2491c65590d8b67ed4c7da378
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-09-24 14:44:23 +0300

    task 8

---
+++ Conditional Statements. Nested Conditional Statements/08. On Time For Exam/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.Design;
+
+namespace _08._On_Time_For_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int exH = int.Parse(Console.ReadLine());
+            int exM = int.Parse(Console.ReadLine());
+            int arH = int.Parse(Console.ReadLine());
+            int arM = int.Parse(Console.ReadLine());
+
+            int arTotalM = arH * 60 + arM;
+            int exTotalM = exH * 60 + exM;
+
+            int diff =  exTotalM - arTotalM ;
+
+            string status = "";
+            string comparison = "";
+
+
+            if (diff >= 0 && diff <= 30)
+            {
+                status = "On time";
+                comparison = "before";
+            }
+            else if (diff < 0)
+            {
+                status = "Late";
+                comparison = "after";
+                diff = -diff;
+            }
+            else
+            {
+                status = "Early";
+                comparison = "before";
+            }
+
+            int compH = diff / 60;
+            int compM = diff % 60;
+            Console.WriteLine(status);
+            if (compH > 0 )
+                Console.WriteLine($"{compH}:{compM:d2} hours {comparison} the start");
+            else if (compM > 0 )
+                Console.WriteLine($"{compM} minutes {comparison} the start");
+ 
+        }
+    }
+}
