ad19b39693b6b983e55612a48c2f14e2722fb017
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-03 17:50:53 +0300

    task 1

---
+++ Simple Loops/01. Numbers Ending in 7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 7; i <= 997; i++) 
+           {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+           }
+        }
+    }
+}
---
+++ Simple Loops/02. Half Sum Element/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02._Half_Sum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
