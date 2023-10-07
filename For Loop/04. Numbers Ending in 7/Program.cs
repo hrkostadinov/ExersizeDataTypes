08b2302666569b953899504f6454d5f78ccd67b8
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-07 13:55:43 +0300

    task-4

---
+++ For Loop/04. Numbers Ending in 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _04._Numbers_Ending_in_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int num = int.Parse(Console.ReadLine());
+            //double sum = 0;
+            for (int i = 7; i <= num; i +=10 )
+            {
+                //sum = sum 
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
