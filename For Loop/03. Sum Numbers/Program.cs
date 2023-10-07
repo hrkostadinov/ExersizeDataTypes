8b39c39a5b3488f439b8721870d6e5bf2667bfb1
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-07 12:36:57 +0300

    task-3

---
+++ For Loop/03. Sum Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            double sum = 0;
+           
+            for(int i = 1; i <= num; i++)
+            {
+                double inNum = double.Parse(Console.ReadLine());
+                sum += inNum;
+            }
+            Console.WriteLine(sum);
+
+
+        }
+
+    }
+}
