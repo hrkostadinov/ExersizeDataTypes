e7b27dea6b2a4f8fc4278d803f4bcc37f83e6754
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-07 17:19:06 +0300

    task-6

---
+++ For Loop/06. Exam Countdown/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _06._Exam_Countdown
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            //int dayCount = 0;
+
+            for(int i = day; i >= 1; i--)
+            {
+                //dayCount += i;
+                Console.WriteLine($"{i} days before the exam");
+                
+            }
+            Console.WriteLine("The exam has come");
+
+        }
+    }
+}
