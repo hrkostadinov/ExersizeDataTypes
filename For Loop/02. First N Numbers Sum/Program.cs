fa608454c3a9418a4d98fef15a7797aac3323e6b
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-07 11:17:02 +0300

    task-2

---
+++ For Loop/02. First N Numbers Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02._First_N_Numbers_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= num; i++)
+            { 
+                sum += i;
+                Console.Write(i);
+                if (i <= num - 1)
+                {
+                    Console.Write("+");
+                }
+                else if (i == num)
+                {
+                    Console.Write($"={sum}");
+                }
+               
+
+            }
+        }
+    }
+}
