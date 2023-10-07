290092ba595df322a6ad0df16d5a9472c34c8c19
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-07 09:54:29 +0300

    task-1

---
+++ For Loop/01. Numbers in Range/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Numbers_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            for(int i = num1; i <= num2; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
