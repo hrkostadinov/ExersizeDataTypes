45383bad828afab53e7bb64bb7ebb7d340701513
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-08 11:13:43 +0300

    task-2

---
+++ While Loop/02. Odd Number/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02._Odd_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            while (num % 2 == 0)
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
