a8d52e6a37500bccc6927d7bbc0cc9dd84d60b4b
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-08 10:35:49 +0300

    task-7

---
+++ For Loop/07. Latin Letters/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07._Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+
+            for (char c = first; c <= second; c++)
+            {
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
