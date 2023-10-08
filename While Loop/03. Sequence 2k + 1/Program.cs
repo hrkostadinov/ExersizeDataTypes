d880f82239cc1f58705c8790445e517ea56af938
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-08 12:09:57 +0300

    task-3

---
+++ While Loop/03. Sequence 2k + 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Sequence_2k___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int i = 1;
+
+            while (num >= i)
+            {
+                Console.WriteLine(i);
+                i = i * 2 + 1;
+            }
+        }
+    }
+}
