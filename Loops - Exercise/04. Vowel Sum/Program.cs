403716b4f39024c71435c97a44a7fdb62b3c2ea7
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-09 19:04:50 +0300

    task-4

---
+++ Loops - Exercise/04. Vowel Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _04._Vowel_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= count; i++)
+            {
+                char simbol = char.Parse(Console.ReadLine());
+
+                switch(simbol)
+                {
+                    case 'a':
+                        sum += 1;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
