458b03b0520010b9777dc894f93a35597ab70c9f
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-10 19:17:36 +0300

    Task-8

---
+++ Loops - Exercise/07. Special Bonus/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _07._Special_Bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stopNum = int.Parse(Console.ReadLine());
+            int preventNum = stopNum;
+            
+
+            while (true)
+            {
+                int num = int.Parse(Console.ReadLine()) ;
+                if (num == stopNum)
+                {
+                   
+                    Console.WriteLine(preventNum * 1.2);
+                    break;
+                }
+
+                preventNum = num;
+            }
+        }
+    }
+}
