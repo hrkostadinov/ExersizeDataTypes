889a7ab688073cf7a0a8d3e573174aa36c8fabaf
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-10 09:21:03 +0300

    task-6

---
+++ Loops - Exercise/06. Special Number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Special_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int startNumber = number;
+            bool isSpecial = true;
+
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+
+                if (startNumber % lastDigit != 0)
+                {
+                    isSpecial = false;
+                    break;
+                }
+                number = number / 10 ;
+            }
+            if (isSpecial == true)
+            {
+                Console.WriteLine(startNumber + " is special");
+            }
+            else 
+            {
+                Console.WriteLine(startNumber + " is not special");
+            }
+        }
+    }
+}
