e93044e3a0120609320089c1428710c73848a3e6
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-12 13:06:04 +0300

    task-2

---
+++ Lab Nested Loops/02. Even Powers of 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02._Even_Powers_of_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter an integer number: ");
+            if (int.TryParse(Console.ReadLine(), out int n) && n >= 0)
+            {
+                for (int power = 0; power <= n; power += 2)
+                {
+                    long result = (long)Math.Pow(2, power);
+                    Console.WriteLine( result);
+                }
+            }
+            
+        }
+    }
+}
