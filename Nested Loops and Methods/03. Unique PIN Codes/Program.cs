68ee0df8ec2e97e26bec18b1fa2e266d78549f03
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-22 18:20:17 +0300

    Task-3

---
+++ Nested Loops and Methods/03. Unique PIN Codes/Program.cs
@@ -0,0 +1,22 @@
+﻿int max1 = int.Parse(Console.ReadLine());
+int max2 = int.Parse(Console.ReadLine());
+int max3 = int.Parse(Console.ReadLine());
+
+// chetno chislo 
+for (int firstDigit = 2; firstDigit <= max1; firstDigit += 2)
+{
+   for (int secondDigit = 1; secondDigit <= max2; secondDigit++)
+    {
+        if (secondDigit == 2 || secondDigit == 3 || secondDigit == 7 || secondDigit == 5)
+        {
+            for (int thirdDigit = 2; thirdDigit <= max3; thirdDigit += 2)
+            {
+                Console.WriteLine($"{firstDigit}{secondDigit}{thirdDigit}");
+            }
+        }
+        
+    }
+}
+
+
+// chetno chislo 
