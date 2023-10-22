ff0c923357641da03335d78947481accd471d9ea
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-22 15:49:38 +0300

    Task-1

---
+++ Lab Nested Loops/03. Triangle of Stars/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _03._Triangle_of_Stars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= size; i++)
+            {
+                for ()
+               
+            }
+        }    
+    }
+}
