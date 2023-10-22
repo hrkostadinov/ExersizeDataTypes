abd7da53de517d6f7c6d09f9d5da7a6a8d827810
hrkostadinov <135539852+hrkostadinov@users.noreply.github.com>
2023-10-22 16:27:22 +0300

    Task-2

---
+++ Nested Loops and Methods/02. Magic Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int firstNumber = 1; firstNumber <= 9; firstNumber++)
+{
+    for (int secondNumber = 0; secondNumber <= 9; secondNumber++)
+    {
+        for (int thirdNumber = 0; thirdNumber <= 9; thirdNumber++)
+        {
+            if (firstNumber * secondNumber * thirdNumber == n)
+            {
+                Console.Write($"{firstNumber}{secondNumber}{thirdNumber} ");
+            }
+            
+        }
+    }
+}
